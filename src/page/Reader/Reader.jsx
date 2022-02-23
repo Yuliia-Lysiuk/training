8a4f2767aae2065534b76c9d3bcf2156c13d9760
Yuliia-Lysiuk <84859026+Yuliia-Lysiuk@users.noreply.github.com>
2022-02-23 10:41:05 +0200

    Update Reader.jsx

--- src/page/Reader/Reader.jsx
+++ src/page/Reader/Reader.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react"
-import { Link, useSearchParams } from "react-router-dom";
+import { Link } from "react-router-dom";
 import { getPublication, deletePublication} from "services/API";
 
 export function Reader({data}) {
